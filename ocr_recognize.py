23f588504de85e5435ee23c7d0bd6f69fe50e917
Pavel Dvorsky <DvorskyPaja@gmail.com>
2020-10-11 21:54:34 +0200

    ocr_recognize - reworked to use functions

--- ocr_recognize.py
+++ ocr_recognize.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
 import pytesseract
-import sys, re
+import sys, re, os
 from openpyxl import load_workbook
 from PIL import Image
-img =Image.open ("/home/pi/Scripts/recognize.png")
 
-custom_config = r"-c tessedit_char_whitelist=02345678 --psm 6 -l digits"
-#custom_config = r'--oem 3 --psm 6 outputbase digits'
+def init():
+    global img =Image.open (os.path.join(os.getcwd() ,"recognize.png"))
+    #print (os.path.join(os.getcwd() ,"recognize.png"))
+def recognize():
+    custom_config = r"-c tessedit_char_whitelist=0123456789 --psm 6 -l digits"
+    #custom_config = r'--oem 3 --psm 6 outputbase digits'
 
-recognized = pytesseract.image_to_string(img, config = custom_config)
-print (recognized)
-text = pytesseract.image_to_data(img, config=custom_config)
-print (text)
-replaced = recognized.replace(' ', '')
-print (replaced)
-actualConsumption = float(replaced)/1000 
-print(actualConsumption)
+    recognized = pytesseract.image_to_string(img, config = custom_config)
+    print (recognized)
+    text = pytesseract.image_to_data(img, config=custom_config)
+    #print (text)
+    replaced = recognized.replace(' ', '')
+    #print (replaced)
+    print(actualConsumption)
+    return actualConsumption = float(replaced)/1000 
+
+# Writing into txt for web
+def writeIntoTxt (file, actualConsumption):
+    file = open(file, 'a')  
+    file.write('\n'+ date +"\t" + time + "\t" + str(actualConsumption))
+    file.close()
+
+init()
+writeIntoTxt('/home/pi/Scripts/web/text.txt', recognize())
 
 # writing into xlsx
-
-from openpyxl import load_workbook
 wb = load_workbook('/home/pi/Desktop/technicka.xlsx')
 ws = wb['Sheet']
 
@@ -28,7 +38,7 @@ row_count = ws.max_row
 column_count = ws.max_column
 writeTo = 'C' + str(row_count+1)
 
-print 'Argument List:', str(sys.argv[1])
+print ('Argument List:', str(sys.argv[1])
 result = re.search(r'\/([0-9|_]+)\.\w+$', str(sys.argv[1]))
 fullDateTime = str(result.group(1))
 fullDateTime = re.split('_', fullDateTime)
@@ -42,18 +52,6 @@ ws[('C' + str(row_count+1))] = str(actualConsumption)
 ws[('A' + str(row_count+1))] = date
 ws[('B' + str(row_count+1))] = time
 
-
 text = ws['A1'].value
 print (text)
 wb.save('/home/pi/Desktop/technicka.xlsx')
-
-file = open('/home/pi/Scripts/web/text.txt', 'a')  
-
-
-file.write('\n'+ date +"\t" + time + "\t" + str(actualConsumption))
-
-file.close()
-
-
-
-
